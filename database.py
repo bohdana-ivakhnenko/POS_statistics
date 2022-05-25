9e7821933e5dd7128a820575053de6bb52a8eca2
bohdana-ivakhnenko <ivahnenko.bogdana@gmail.com>
2022-05-25 23:56:15 +0200

    add reading function

--- database.py
+++ database.py
@@ -1,10 +1,21 @@
 import sqlite3
 
 
+def commit(db_function, self):
+    def db_decorator(function):
+        function()
+        self.conn.commit()
+    return db_decorator(db_function)
+
+
 class Database:
     def __init__(self, name: str):
         self.name = name
-        self.c = sqlite3.connect(self.name).cursor()
+        self.conn = sqlite3.connect(self.name)
+        self.c = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
 
     def create_table(self, table_name: str, columns: dict, autoincrement=True, drop=False) -> str:
         if drop:
@@ -22,8 +33,26 @@ class Database:
         self.c.execute(query)
         return query
 
-    def read_from_table(self, name, columns, where, num, order_by) -> str:
-        query = ""
+    def read_from_table(self, name: str, columns: tuple, where, num, order_by, desc=True) -> str:
+        if columns:
+            columns = f"({' ,'.join(columns).strip(' ,')})"
+        else:
+            columns = "*"
+
+        query = f"SELECT {columns} FROM {name}"
+
+        if where:
+            query = query + f" WHERE {where}"
+
+        if order_by:
+            order = {True: " DESC",
+                    False: ""}
+            query = query + f" ORDER BY {order_by}{order[desc]}"
+
+        if num:
+            query = query + f" LIMIT {num}"
+
+        self.c.execute(query)
         return query
 
     def insert_into_table(self):
