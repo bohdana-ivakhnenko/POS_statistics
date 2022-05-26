7959d4b9836dfa7a5024ff81b71c01adcb5beaae
bohdana-ivakhnenko <ivahnenko.bogdana@gmail.com>
2022-05-26 14:08:30 +0200

    add sceleton

---
+++ processing.py
@@ -0,0 +1,22 @@
+import tokenize_uk
+import stanza
+
+
+def into_sentences_table(text: str) -> set:
+    pass
+
+
+def divide_into_subsamples(clear_tokens: list) -> list:
+    pass
+
+
+def into_word_forms_table(sentences: tuple) -> set:
+    pass
+
+
+def into_lemmas_table(word_forms: tuple) -> set:
+    pass
+
+
+def into_pos_table(lemmas: tuple) -> set:
+    pass
