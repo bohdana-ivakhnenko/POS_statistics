9d365c889f05cec093d9952acd3993d6538cc2bb
bohdana-ivakhnenko <ivahnenko.bogdana@gmail.com>
2022-05-26 14:09:02 +0200

    add function on creating all the tables

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import processing, database
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_tables(data_types: tuple, db, sentences=True, word_forms=True, lemmas=True, pos=True):
+    for type_ in data_types:
+        with open(f"tales_{type_}.txt", "r", encoding="utf-8") as file:
+            text = file.read()
+
+        if sentences:
+            sentences_table = processing.into_sentences_table(text)
+            db.create_table(f"sentences_{type_}", sentences_table, autoincrement=False)
+        else:
+            sentences_table = db.read_from_table(f"sentences_{type_}")
+        print("Finished on SENTENCES")
+
+        if word_forms:
+            word_forms_table = processing.into_word_forms_table(sentences_table)
+            db.create_table(f"word_forms_{type_}", word_forms_table, autoincrement=False)
+        else:
+            word_forms_table = db.read_from_table(f"word_forms_{type_}")
+        print("Finished on WORD FORMS")
+
+        if lemmas:
+            lemmas_table = processing.into_lemmas_table(word_forms_table)
+            db.create_table(f"lemmas_{type_}", lemmas_table, autoincrement=False)
+        else:
+            lemmas_table = db.read_from_table(f"lemmas_{type_}")
+        print("Finished on LEMMAS")
+
+        if pos:
+            pos_table = processing.into_pos_table(lemmas_table)
+            db.create_table(f"pos_{type_}", pos_table, autoincrement=False)
+        print("Finished on POS")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    db = database.Database("tales.db")
+    types_of_tales = ("authors", "folk")
+    create_tables(types_of_tales, db)
