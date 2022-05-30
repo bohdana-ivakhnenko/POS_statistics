29ff5222b2c092af19bbec9c26b4ebc7a3cc4ffc
bohdana-ivakhnenko <ivahnenko.bogdana@gmail.com>
2022-05-31 00:22:51 +0200

    add pos filtering

--- processing.py
+++ processing.py
@@ -80,7 +80,7 @@ def into_lemmas_table(word_forms: tuple, auto_num=True) -> set:
     return set(create_numeration(lemmas_lines))
 
 
-def into_pos_table(lemmas: tuple, auto_num=True) -> set:
+def into_pos_table(lemmas: tuple, auto_num=True, filter_=False) -> set:
     add = 1
     if not auto_num or type(lemmas[0][0]) == int:
         add = 2
@@ -90,16 +90,44 @@ def into_pos_table(lemmas: tuple, auto_num=True) -> set:
     # columns = (('id', 'INT'), ('lemma', 'TEXT'), ('pos', 'TEXT'), ('abs_freq', 'INT'))
     # columns = (('id', 'INT'), ('pos', 'TEXT'), ('abs_freq', 'INT'))
 
+    filter_dict = {
+        "NOUN": "NOUN",
+        "ADV": "ADV",
+        "VERB": "VERB",
+        "PART": "PART_INTJ",
+        "ADJ": "ADJ",
+        "NUM": "NUM",
+        "ADP": "PREP",
+        "CCONJ": "CONJ",
+        "SCONJ": "CONJ",
+        "PRON": "NOUN",
+        "DET": "ADJ",
+        "INTJ": "PART_INTJ",
+        "PROPN": "NOUN",
+        "PUNCT": "PART_INTJ",
+        "X": "X"
+    }
+
     for lemma in lemmas:
-        word_info = lemma[add:add + 1]
+        lemma_ = list(lemma).copy()
+
+        if filter_:
+            if lemma_[2] == "AUX" and lemma_[1] in ['б', 'би']:
+                lemma_[2] = "PART_INTJ"
+            elif lemma_[2] == "AUX" and lemma_[1] == 'бути':
+                lemma_[2] = "VERB"
+            else:
+                lemma_[2] = filter_dict[lemma_[2]]
+
+        word_info = tuple(lemma_[add:add + 1])
 
         if word_info not in unique_pos:
             unique_pos.append(word_info)
-            pos_lines.append(list(lemma[add:]))
+            pos_lines.append(list(lemma_[add:]))
 
         else:
             index = unique_pos.index(word_info)
-            for num, el in enumerate(lemma[add + 1:]):
+            for num, el in enumerate(lemma_[add + 1:]):
                 pos_lines[index][1 + num] += el
 
     if auto_num:
